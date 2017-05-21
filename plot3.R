2792ccb1cd9338aaeb80108b78c55b4f5f9ae473
BrigitteW <brigitte.wetz@gmail.com>
2017-05-20 20:35:01 -0500

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,9 @@
+data<-read.table("household_power_consumption.txt", header=FALSE,na.strings="?",sep=";",skip=66637,nrows=2880,col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+as.Date(data$Date,format="%d/%m/%Y")
+DayTime<-strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")
+png(filename="plot3.png", width=480, height=480)
+plot(DayTime,data$Sub_metering_1, type="l",ylab="Energy sub metering",xlab="")
+lines(DayTime,data$Sub_metering_2, col="red", type="l")
+lines(DayTime,data$Sub_metering_3, col="blue", type="l")
+legend("topright",lty=1, lwd= 1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off
